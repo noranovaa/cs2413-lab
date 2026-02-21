e24ad8d10b643f8315e1ac4d6f3d8d56397a872d
noranovaa <noraanya15@gmail.com>
2026-02-20 23:14:11 -0600

    Complete Assignment 1

--- Assignment1/Question2/student.c
+++ Assignment1/Question2/student.c
@@ -15,8 +15,14 @@
 
 
 int majorityElement(int* nums, int numsSize) {
-     // TODO: implement
-
-
-
+    int count = 0;
+    int candidate = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+        }
+        count += (nums[i] == candidate) ? 1 : -1;
+    }
+    return candidate;
+     
 }
