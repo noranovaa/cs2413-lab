43ee051718a300480777e9a4038a122511ed1369
noranovaa <noraanya15@gmail.com>
2026-04-09 14:14:36 -0500

    lab05 and lab06 complete

--- lab06/Question1/student.c
+++ lab06/Question1/student.c
@@ -5,32 +5,22 @@
 // Write a function that counts how many edges are in an undirected graph.
 // The graph is stored as an adjacency matrix. (In this lab, we use an adjacency matrix.)
 // Graph* g is a pointer to a Graph struct, which contains a 2D array adj[MAX_NODES][MAX_NODES]. 
-// The value at adj[u][v] is 1 if there is an edge between nodes u and v, and 0 otherwise.
-
-
+// The value at adj[u][v] is 1 if there is an edge between nodes u and v, and 0 otherwise
 
 
 int count_edges(Graph* g) {
-    // TODO: implement
-    // return -1;
+    int count = 0;
 
+    for (int i = 0; i < MAX_NODES; i++) {
+        for (int j = i + 1; j < MAX_NODES; j++) {
+            if (g->adj[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+
+    return count;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- lab06/Question2/student.c
+++ lab06/Question2/student.c
@@ -8,26 +8,26 @@
 // The value at adj[u][v] is 1 if there is an edge between nodes u and v, and 0 otherwise.
 
 
-
-
 int count_isolated(Graph* g) {
-    // TODO: implement
-    // return -1;
-   
+    int count = 0;
+
+    for (int i = 0; i < MAX_NODES; i++) {
+        int isolated = 1;
+
+        for (int j = 0; j < MAX_NODES; j++) {
+            if (g->adj[i][j] != 0) {
+                isolated = 0;
+                break;
+            }
+        }
+
+        if (isolated) {
+            count++;
+        }
+    }
+
+    return count;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Tip: In the adjacency matrix, that means the entire row for that node contains only 0s.
